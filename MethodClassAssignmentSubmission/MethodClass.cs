15876e54cdaceccafcc2f54fcae657b4144eb85d
CosmoCat1111 <131853637+CosmoCat1111@users.noreply.github.com>
2024-06-17 11:20:43 -0700

    Add project files.

---
+++ MethodClassAssignmentSubmission/MethodClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignmentSubmission
+{
+    class Program
+    {
+              static void Main()
+            {
+                MathOperations mathOps = new MathOperations();
+
+                mathOps.PerformOperation(10, 5);
+                //  method with two integers 
+                mathOps.PerformOperation(num1: 7, num2: 3);
+                // method showing number
+                Console.WriteLine("Press any key to exit....");
+                Console.ReadLine();
+        }
+    }
+}
+
---
+++ MethodClassAssignmentSubmission/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class MathOperations
+{ 
+       public void PerformOperation(int num1, int num2)
+        {
+        // method 1
+        int result = 2 * num1;
+        // method 2x
+        Console.WriteLine("The integer number is : " + num2);
+        }
+}
+
+
+
+    
+
